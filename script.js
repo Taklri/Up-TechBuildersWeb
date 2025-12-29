972ac3dd982d737aa0b34dbe47adf6da6f561944
Frankie Ignacio <ignaciofrankie03@gmail.com>
2025-12-30 01:20:08 +0800

    finishedtext

---
+++ script.js
@@ -0,0 +1,28 @@
+  const slider = document.querySelector('.slider');
+  const slides = document.querySelectorAll('.slide');
+  const prev = document.querySelector('.prev');
+  const next = document.querySelector('.next');
+
+  let index = 0;
+  const slidesToShow = 3;
+
+  function updateSlider() {
+    const slideWidth = slides[0].clientWidth;
+    slider.style.transform = `translateX(-${index * slideWidth}px)`;
+  }
+
+  next.addEventListener('click', () => {
+    if (index < slides.length - slidesToShow) {
+      index++;
+      updateSlider();
+    }
+  });
+
+  prev.addEventListener('click', () => {
+    if (index > 0) {
+      index--;
+      updateSlider();
+    }
+  });
+
+  window.addEventListener('resize', updateSlider);
