3dadd6a9dc3205ca9e61ed7f4c4c93cd70e58a6c
Frankie Ignacio <ignaciofrankie03@gmail.com>
2025-12-30 13:45:15 +0800

    Added-Responsiveness

--- script.js
+++ script.js
@@ -26,3 +26,10 @@
   });
 
   window.addEventListener('resize', updateSlider);
+ 
+const burger = document.getElementById("burger");
+const navLinks = document.getElementById("nav-links");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
